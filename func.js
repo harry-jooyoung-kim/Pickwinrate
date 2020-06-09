4c6fedf80ee962ca9c9ceaf7d6c1eaafdaeb6017
unknown <cjsdntlswhz34@gmail.com>
2020-06-09 11:32:26 +0900

    first add

---
+++ func.js
@@ -0,0 +1,253 @@
+var champlist=[{'name':'아트록스','code':'266'},
+{'name':'아리','code':'103'},
+{'name':'아칼리','code':'84'},
+{'name':'알리스타','code':'12'},
+{'name':'아무무','code':'32'},
+{'name':'애니비아','code':'34'},
+{'name':'애니','code':'1'},
+{'name':'아펠리오스','code':'523'},
+{'name':'애쉬','code':'22'},
+{'name':'아우렐리온 솔','code':'136'},
+{'name':'아지르','code':'268'},
+{'name':'바드','code':'432'},
+{'name':'블리츠크랭크','code':'53'},
+{'name':'브랜드','code':'63'},
+{'name':'브라움','code':'201'},
+{'name':'케이틀린','code':'51'},
+{'name':'카밀','code':'164'},
+{'name':'카시오페아','code':'69'},
+{'name':'초가스','code':'31'},
+{'name':'코르키','code':'42'},
+{'name':'다리우스','code':'122'},
+{'name':'다이애나','code':'131'},
+{'name':'드레이븐','code':'119'},
+{'name':'문도 박사','code':'36'},
+{'name':'에코','code':'245'},
+{'name':'엘리스','code':'60'},
+{'name':'이블린','code':'28'},
+{'name':'이즈리얼','code':'81'},
+{'name':'피들스틱','code':'9'},
+{'name':'피오라','code':'114'},
+{'name':'피즈','code':'105'},
+{'name':'갈리오','code':'3'},
+{'name':'갱플랭크','code':'41'},
+{'name':'가렌','code':'86'},
+{'name':'나르','code':'150'},
+{'name':'그라가스','code':'79'},
+{'name':'그레이브즈','code':'104'},
+{'name':'헤카림','code':'120'},
+{'name':'하이머딩거','code':'74'},
+{'name':'일라오이','code':'420'},
+{'name':'이렐리아','code':'39'},
+{'name':'아이번','code':'427'},
+{'name':'잔나','code':'40'},
+{'name':'자르반 4세','code':'59'},
+{'name':'잭스','code':'24'},
+{'name':'제이스','code':'126'},
+{'name':'진','code':'202'},
+{'name':'징크스','code':'222'},
+{'name':'카이사','code':'145'},
+{'name':'칼리스타','code':'429'},
+{'name':'카르마','code':'43'},
+{'name':'카서스','code':'30'},
+{'name':'카사딘','code':'38'},
+{'name':'카타리나','code':'55'},
+{'name':'케일','code':'10'},
+{'name':'케인','code':'141'},
+{'name':'케넨','code':'85'},
+{'name':'카직스','code':'121'},
+{'name':'킨드레드','code':'203'},
+{'name':'클레드','code':'240'},
+{'name':'코그모','code':'96'},
+{'name':'르블랑','code':'7'},
+{'name':'리 신','code':'64'},
+{'name':'레오나','code':'89'},
+{'name':'리산드라','code':'127'},
+{'name':'루시안','code':'236'},
+{'name':'룰루','code':'117'},
+{'name':'럭스','code':'99'},
+{'name':'말파이트','code':'54'},
+{'name':'말자하','code':'90'},
+{'name':'마오카이','code':'57'},
+{'name':'마스터 이','code':'11'},
+{'name':'미스 포츈','code':'21'},
+{'name':'오공','code':'62'},
+{'name':'모데카이저','code':'82'},
+{'name':'모르가나','code':'25'},
+{'name':'나미','code':'267'},
+{'name':'나서스','code':'75'},
+{'name':'노틸러스','code':'111'},
+{'name':'니코','code':'518'},
+{'name':'니달리','code':'76'},
+{'name':'녹턴','code':'56'},
+{'name':'누누와 윌럼프','code':'20'},
+{'name':'올라프','code':'2'},
+{'name':'오리아나','code':'61'},
+{'name':'오른','code':'516'},
+{'name':'판테온','code':'80'},
+{'name':'뽀삐','code':'78'},
+{'name':'파이크','code':'555'},
+{'name':'키아나','code':'246'},
+{'name':'퀸','code':'133'},
+{'name':'라칸','code':'497'},
+{'name':'람머스','code':'33'},
+{'name':'렉사이','code':'421'},
+{'name':'레넥톤','code':'58'},
+{'name':'렝가','code':'107'},
+{'name':'리븐','code':'92'},
+{'name':'럼블','code':'68'},
+{'name':'라이즈','code':'13'},
+{'name':'세주아니','code':'113'},
+{'name':'세나','code':'235'},
+{'name':'세트','code':'875'},
+{'name':'샤코','code':'35'},
+{'name':'쉔','code':'98'},
+{'name':'쉬바나','code':'102'},
+{'name':'신지드','code':'27'},
+{'name':'사이온','code':'14'},
+{'name':'시비르','code':'15'},
+{'name':'스카너','code':'72'},
+{'name':'소나','code':'37'},
+{'name':'소라카','code':'16'},
+{'name':'스웨인','code':'50'},
+{'name':'사일러스','code':'517'},
+{'name':'신드라','code':'134'},
+{'name':'탐 켄치','code':'223'},
+{'name':'탈리야','code':'163'},
+{'name':'탈론','code':'91'},
+{'name':'타릭','code':'44'},
+{'name':'티모','code':'17'},
+{'name':'쓰레쉬','code':'412'},
+{'name':'트리스타나','code':'18'},
+{'name':'트런들','code':'48'},
+{'name':'트린다미어','code':'23'},
+{'name':'트위스티드 페이트','code':'4'},
+{'name':'트위치','code':'29'},
+{'name':'우디르','code':'77'},
+{'name':'우르곳','code':'6'},
+{'name':'바루스','code':'110'},
+{'name':'베인','code':'67'},
+{'name':'베이가','code':'45'},
+{'name':'벨코즈','code':'161'},
+{'name':'바이','code':'254'},
+{'name':'빅토르','code':'112'},
+{'name':'블라디미르','code':'8'},
+{'name':'볼리베어','code':'106'},
+{'name':'워윅','code':'19'},
+{'name':'자야','code':'498'},
+{'name':'제라스','code':'101'},
+{'name':'신 짜오','code':'5'},
+{'name':'야스오','code':'157'},
+{'name':'요릭','code':'83'},
+{'name':'유미','code':'350'},
+{'name':'자크','code':'154'},
+{'name':'제드','code':'238'},
+{'name':'직스','code':'115'},
+{'name':'질리언','code':'26'},
+{'name':'조이','code':'142'},
+{'name':'자이라','code':'143'}
+]
+
+var xvec=[0,0,0,0,0,0,0,0,0,0];
+var model=[0.000187857811290412,0.000284115446158517,0.000294671117808963,0.000257649660336428,0.000169631468466434,0.000181799941195201,0.00028281833596686,0.00259582780560222,0.000280607530912842,0.000194539620434097];
+
+$("#button-ourtop").on("click", function() {
+    for (i = 0; i < champlist.length; i++) {
+        if ($('.Ourtop').val() === champlist[i].name){
+            xvec[0]=champlist[i].code;
+            alert('우리팀 탑이 입력되었습니다.');    
+        }
+    }
+});
+
+$("#button-ourjun").on("click", function() {
+    for (i = 0; i < champlist.length; i++) {
+        if ($('.Ourjun').val() === champlist[i].name){
+            xvec[1]=champlist[i].code;
+            alert('우리팀 정글이 입력되었습니다.');    
+        }
+    }
+});
+$("#button-ourmid").on("click", function() {
+    for (i = 0; i < champlist.length; i++) {
+        if ($('.Ourmid').val() === champlist[i].name){
+            xvec[2]=champlist[i].code;
+            alert('우리팀 미드가 입력되었습니다.');    
+        }
+    }
+});
+$("#button-ouradc").on("click", function() {
+    for (i = 0; i < champlist.length; i++) {
+        if ($('.Ouradc').val() === champlist[i].name){
+            xvec[3]=champlist[i].code;
+            alert('우리팀 원딜이 입력되었습니다.');    
+        }
+    }
+});
+$("#button-oursup").on("click", function() {
+    for (i = 0; i < champlist.length; i++) {
+        if ($('.Oursup').val() === champlist[i].name){
+            xvec[4]=champlist[i].code;
+            alert('우리팀 서포터가 입력되었습니다.');    
+        }
+    }
+});
+$("#button-enmtop").on("click", function() {
+    for (i = 0; i < champlist.length; i++) {
+        if ($('.Enmtop').val() === champlist[i].name){
+            xvec[5]=champlist[i].code;
+            alert('상대팀 탑이 입력되었습니다.');    
+        }
+    }
+});
+$("#button-enmjun").on("click", function() {
+    for (i = 0; i < champlist.length; i++) {
+        if ($('.Enmjun').val() === champlist[i].name){
+            xvec[6]=champlist[i].code;
+            alert('상대팀 정글이 입력되었습니다.');    
+        }
+    }
+});
+$("#button-enmmid").on("click", function() {
+    for (i = 0; i < champlist.length; i++) {
+        if ($('.Enmmid').val() === champlist[i].name){
+            xvec[7]=champlist[i].code;
+            alert('상대팀 미드가 입력되었습니다.');    
+        }
+    }
+});
+$("#button-enmadc").on("click", function() {
+    for (i = 0; i < champlist.length; i++) {
+        if ($('.Enmadc').val() === champlist[i].name){
+            xvec[8]=champlist[i].code;
+            alert('상대팀 원딜이 입력되었습니다.');    
+        }
+    }
+});
+$("#button-enmsup").on("click", function() {
+    for (i = 0; i < champlist.length; i++) {
+        if ($('.Enmsup').val() === champlist[i].name){
+            xvec[9]=champlist[i].code;
+            alert('상대팀 서포터가 입력되었습니다.');    
+        }
+    }
+});
+
+
+
+
+
+function Prediction() {
+    var result = document.getElementById("result");
+    var output=0;
+    
+    for (i = 0; i < 10; i++) {
+        output+=xvec[i]*model[i];
+    }
+
+    if (output>0.5){
+    result.innerText = "승리할 확률이 높습니다!\n"+output;
+    } else{
+    result.innerText = "패배할 확률이 높습니다!\n"+output;
+    }
+};
